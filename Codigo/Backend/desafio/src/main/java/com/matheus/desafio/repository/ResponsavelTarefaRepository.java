8719d1bf3c39f111918ca5991c27c49cc1c17575
matheusmt12 <matheusdacruzsouza1998@gmail.com>
2025-01-21 12:01:48 -0300

    Criacao do repository de responsavel tarefa

---
+++ Codigo/Backend/desafio/src/main/java/com/matheus/desafio/repository/ResponsavelTarefaRepository.java
@@ -0,0 +1,16 @@
+package com.matheus.desafio.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.matheus.desafio.entity.ResponsavelTarefa;
+
+@Repository
+public interface ResponsavelTarefaRepository extends JpaRepository<ResponsavelTarefa, Integer> {
+
+    @Query(value = "SELECT * FROM  TB_RESPONSAVEL_TAREFA", nativeQuery = true)
+    List<ResponsavelTarefa> getAll();
+}
