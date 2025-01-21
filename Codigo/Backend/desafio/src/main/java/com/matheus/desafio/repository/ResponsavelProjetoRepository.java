9297496a30e5df8f41513218e41babb03aaaa746
matheusmt12 <matheusdacruzsouza1998@gmail.com>
2025-01-21 11:59:43 -0300

    Criacao do repository de Responsavel Projeto

---
+++ Codigo/Backend/desafio/src/main/java/com/matheus/desafio/repository/ResponsavelProjetoRepository.java
@@ -0,0 +1,17 @@
+package com.matheus.desafio.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.matheus.desafio.entity.ResponsavelProjeto;
+
+@Repository
+public interface ResponsavelProjetoRepository  extends JpaRepository<ResponsavelProjeto, Integer>{
+
+
+    @Query( value = "SELECT * FROM TB_RESPONSAVEL_PROJETO" , nativeQuery = true)
+    List<ResponsavelProjeto> getAll();
+}
