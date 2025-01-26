d898ebad1d764004acf1d7b44828681a9520cf84
matheusmt12 <matheusdacruzsouza1998@gmail.com>
2025-01-26 10:15:13 -0300

    criando a rota para o projeto e suas tarefas

--- Codigo/Frontend/desafio-front/src/router/index.js
+++ Codigo/Frontend/desafio-front/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import ProjetoView from '@/views/ProjetoView.vue'
+import TarefaView from '@/views/TarefaView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,15 +16,20 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue'),
-    },{
-      path : '/login',
-      name : 'login',
-      component : LoginView,
+    }, {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
     },
     {
-      path : '/projeto',
-      name : 'projeto',
-      component : ProjetoView
+      path: '/projeto',
+      name: 'projeto',
+      component: ProjetoView,
+    },
+    {
+      path : '/tarefas/:id',
+      name : 'tarefas',
+      component : TarefaView
     }
   ],
 })
--- Codigo/Frontend/desafio-front/src/views/ProjetoView.vue
+++ Codigo/Frontend/desafio-front/src/views/ProjetoView.vue
@@ -5,14 +5,18 @@ import { onMounted, ref } from 'vue';
 import CardComponent from '@/components/CardComponent.vue';
 import TableComponent from '@/components/TableComponent.vue';
 import NavbarComponent from '@/components/NavbarComponent.vue';
+import { RouterView } from 'vue-router';
+import router from '@/router';
 
 
 //variáveis 
 const url = "http://localhost:8080/projeto";
-
 const dadosProjeto = ref([]);
 const nomePesquisa = ref("");
-const buscarProjetos = () => {
+const tarefas = ref(true);
+
+//funções
+function buscarProjetos() {
 
     let token = localStorage.getItem('token');
 
@@ -30,23 +34,34 @@ const buscarProjetos = () => {
 
 }
 
+function pesquisarNome(params) {
+    nomePesquisa.value = params;
+    console.log(nomePesquisa.value);
+
+}
+
+//metodo para redirecionar para as tarefas do projeto
+function tarefa(id) {
+    router.push('/tarefas/'+id)
+    
+}
+
+
 onMounted(() => {
     buscarProjetos();
 });
 
 
-function pesquisarNome(params) {
-    nomePesquisa.value = params;
-    console.log(nomePesquisa.value);
-    
-}
+
 
 </script>
 <template>
     <NavbarComponent @pesquisarNome="pesquisarNome"></NavbarComponent>
     <CardComponent titulo="Projetos">
         <template v-slot:conteudo>
-            <TableComponent :dados="dadosProjeto" :titulos="['id', 'nome']"></TableComponent>
+            <TableComponent :dados="dadosProjeto" :titulos="['nome', 'nome_responsavel', 'status', 'data_termino']"
+                :tarefas="tarefas" @funcTarefas="tarefa">
+            </TableComponent>
         </template>
         <template v-slot:footer>
             <button class="btn btn-primary ">teste</button>
