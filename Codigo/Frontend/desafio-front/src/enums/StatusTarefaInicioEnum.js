c20a467e054766a5491fc91d20f3ccc8d1f4e562
matheusmt12 <matheusdacruzsouza1998@gmail.com>
2025-01-26 17:10:46 -0300

    Concluido método de adicionar uma nova tarefa no frontend

---
+++ Codigo/Frontend/desafio-front/src/enums/StatusTarefaInicioEnum.js
@@ -0,0 +1,6 @@
+const StatusTarefaInicioEnum = Object.freeze({
+    PLANEJADO: 'PLANEJADO',
+    EM_EXECUCAO: 'EM EXECUÇÃO',
+});
+
+export default StatusTarefaInicioEnum;
--- Codigo/Frontend/desafio-front/src/views/TarefaView.vue
+++ Codigo/Frontend/desafio-front/src/views/TarefaView.vue
@@ -8,14 +8,32 @@ import TableComponent from '@/components/TableComponent.vue';
 import ModalComponent from '@/components/ModalComponent.vue';
 import InputComponent from '@/components/InputComponent.vue';
 import StatusTarefaEnum from '@/enums/StatusTarefaEnum';
+import StatusTarefaInicioEnum from '@/enums/StatusTarefaInicioEnum';
+import AlertComponent from '@/components/AlertComponent.vue';
 
 
 //variáveis
 const url = "http://localhost:8080/tarefa";
 const idProjeto = router.currentRoute.value.params.id;
 const dadosTarefas = ref([]);
-const statusAlterar = ref(true)
+const dadosResponsaveis = ref([]);
+const statusAlterar = ref(true);
 const tarefa = ref(null);
+
+//variáveis Alert
+
+const sucesso = ref(false);
+const menssagem = ref("");
+const erro = ref(false);
+const statusAlertSucesso = ref("");
+const statusAlertErro = ref("");
+
+
+// variáveis para adicionar uma nova tarefa
+
+const titulo = ref("");
+const descricao = ref("");
+const prazo = ref(0);
 
 //Modal funções e variáveis 
 const visivelModal = ref(false);
@@ -27,6 +45,10 @@ function abrirModal() {
 function fecharModal() {
     visivelModal.value = false;
     visivelModalStatus.value = false;
+    sucesso.value = false;
+    menssagem.value ="";
+    erro.value = false;
+    statusAlertSucesso.value = "";
 }
 
 function mudarStatus(obj) {
@@ -53,10 +75,47 @@ function buscarTarefas() {
     });
 }
 
+function salvarNovaTarefa() {
+    let idResponsavel = document.getElementById('idResponsavel').value;
+    let status = document.getElementById('tarefaStatus').value;
+
+    let data = {
+        titulo: titulo.value,
+        descricao: descricao.value,
+        prazo: prazo.value,
+        status: status,
+        id_responsavel: idResponsavel,
+        id_projeto: idProjeto
+
+    }
+
+    axios.post(url, data, {
+        headers: {
+            'Authorization': 'Bearer ' + token()
+        }
+    }).then(response => {
+
+        sucesso.value = true;
+        menssagem.value = response.data;
+        erro.value = false;
+        statusAlertSucesso.value = "Sucesso";
+
+        buscarTarefas();
+
+
+    }).catch(error => {
+
+        sucesso.value = false;
+        menssagem.value = error.response.data;
+        erro.value = true;
+        statusAlertErro.value = "Erro";
+    })
+}
+
 function alterarStatus() {
     let status = document.getElementById('statusTarefa').value;
-    
-    axios.put(url +'/alterar/' + tarefa.value.id,{
+
+    axios.put(url + '/alterar/' + tarefa.value.id, {
         status: status
     }, {
         headers: {
@@ -65,8 +124,17 @@ function alterarStatus() {
     }
     ).then(response => {
         console.log(response.data);
-        
+        sucesso.value = true;
+        menssagem.value = response.data.message[0];
+        erro.value = false;
+        statusAlertSucesso.value = "Sucesso";
         buscarTarefas();
+    }).catch(error => {
+
+        sucesso.value = false;
+        menssagem.value = error.response.message;
+        erro.value = true;
+        statusAlertErro.value = "Erro";
     })
 
 }
@@ -75,6 +143,18 @@ function alterarStatus() {
 
 onMounted(() => {
     buscarTarefas();
+    const buscarResponsaveis = () => {
+        let url = 'http://localhost:8080/responsaveltarefa';
+        axios.get(url, {
+            headers: {
+                'Authorization': 'Bearer ' + token()
+            }
+        }).then(response => {
+            dadosResponsaveis.value = response.data;
+
+        })
+    };
+    buscarResponsaveis();
 })
 </script>
 
@@ -91,11 +171,67 @@ onMounted(() => {
             </span>
         </template>
         <template v-slot:footer>
-            <button class="btn btn-primary">Adicionar</button>
+            <button class="btn btn-primary" @click="abrirModal">Adicionar</button>
         </template>
     </CardComponent>
 
+    <ModalComponent titulo="Nova Tarefa" :visivel="visivelModal">
+        <template v-slot:alert>
+            <AlertComponent v-if="sucesso" :message="menssagem" classAlert="alert alert-success"
+                :status="statusAlertSucesso"></AlertComponent>
+            <AlertComponent v-if="erro" :message="menssagem" classAlert="alert alert-danger" :status="statusAlertErro">
+            </AlertComponent>
+        </template>
+        <template v-slot:conteudo>
+            <div class="row">
+                <div class="col">
+                    <InputComponent label="Titulo" for-id="tituloTarefa">
+                        <input type="text" class="form-control" required v-model="titulo">
+                    </InputComponent>
+                </div>
+                <div class="col">
+                    <InputComponent label="Descrição" for-id="descricaoTarefa">
+                        <input type="text" class="form-control" required v-model="descricao">
+                    </InputComponent>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <InputComponent label="Prazo" for-id="prazoTarefa">
+                        <input type="number" min="1" required v-model="prazo" class="form-control">
+                    </InputComponent>
+                </div>
+                <div class="col">
+                    <InputComponent label="responsavelTarefa" for-id="responsavelTarefa">
+                        <select name="idResponsavel" id="idResponsavel" class="form-control">
+                            <option v-for="i in dadosResponsaveis" :value="i.id">{{ i.nome }}</option>
+                        </select>
+                    </InputComponent>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <InputComponent label="Status">
+                        <select name="tarefaStatus" id="tarefaStatus" class="form-control">
+                            <option v-for="i in StatusTarefaInicioEnum" :value="i">{{ i }}</option>
+                        </select>
+                    </InputComponent>
+                </div>
+            </div>
+        </template>
+        <template v-slot:footer>
+            <button class="btn btn-secondary" @click="fecharModal">Fechar</button>
+            <button class="btn btn-primary" @click="salvarNovaTarefa">Salvar</button>
+        </template>
+    </ModalComponent>
+
     <ModalComponent :visivel="visivelModalStatus" titulo="Alterar Status">
+        <template v-slot:alert>
+            <AlertComponent v-if="sucesso" :message="menssagem" classAlert="alert alert-success"
+                :status="statusAlertSucesso"></AlertComponent>
+            <AlertComponent v-if="erro" :message="menssagem" classAlert="alert alert-danger" :status="statusAlertErro">
+            </AlertComponent>
+        </template>
         <template v-slot:conteudo>
             <div class="row">
                 <div class="col">
@@ -124,7 +260,7 @@ onMounted(() => {
             <h5>Alterar Status</h5>
             <div class="row">
                 <div class="col">
-                    <select name="statusTarefa" id="statusTarefa">
+                    <select name="statusTarefa" id="statusTarefa" class="form-control">
                         <option v-for="status in StatusTarefaEnum" :value="status">{{ status }}</option>
                     </select>
                 </div>
